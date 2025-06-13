108d6dfbd968102a3940c2c0980b5ac4ac7dc468
Lara Klopper <135839853+laraklopper@users.noreply.github.com>
2025-06-13 16:37:09 +0200

    Update Header.js

--- calculator/client/src/components/Header.js
+++ calculator/client/src/components/Header.js
@@ -1,20 +1,22 @@
 import React from 'react'
+// Bootstrap
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+//Header function component
 export default function Header() {
+
+  //===============JSX RENDERING====================
+  
   return (
+    //Header
     <header className='header'>
         <Row className='headerRow'>
-              <Col xs={6} md={4}>
-                
-              </Col>
+              <Col xs={6} md={4}></Col>
               <Col xs={6} md={4} id='headCol'>
                 <h1 id='heading'>CALCULATOR</h1>
             </Col>
-              <Col xs={6} md={4}>
-
-              </Col>
+              <Col xs={6} md={4}> </Col>
         </Row>
     </header>
   )
